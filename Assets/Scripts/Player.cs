d2247625b7450ae442d8e5411b780aa66bbbfe91
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2023-11-24 11:20:51 +0000

    Some player movement added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private bool moveLeft;
+    private bool moveRight;
+    private float horizontalMove;
+    private float speed = 3;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        moveLeft = false;
+        moveRight = false;
+    }
+
+    public void PointerDownLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void PointerUpLeft()
+    {
+        moveLeft = false;
+    }
+
+    public void PointerDownRight()
+    {
+        moveRight = true;
+    }
+
+    public void PointerUpRight()
+    {
+        moveRight = false;
     }
 
     void Update()
     {
-        
+        MovementPlayer();
+    }
+
+    private void MovementPlayer()
+    {
+        if (moveLeft)
+        {
+            horizontalMove = -speed;
+        }
+
+        else if (moveRight)
+        {
+            horizontalMove = speed;
+        }
+
+        else
+        {
+            horizontalMove = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
     }
 }
